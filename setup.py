20a7e99f2318a19dbaf5bf9a4004b270e9de5e49
divython <84345836+divython@users.noreply.github.com>
2025-07-05 20:40:11 +0530

    commit

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="askdev",
     version="1.0.0",
     description="Android Error CLI Assistant",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="askdev contributors",
+    url="https://github.com/divython/askdev-cli",
     packages=find_packages(),
     py_modules=["app"],
     install_requires=[
@@ -15,10 +21,21 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "askdev=app:run_main"
+            "askdev=app:run_main",
+            "ad=app:run_main"
         ]
     },
     include_package_data=True,
     package_data={"": ["error_db.json"]},
     python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Bug Tracking",
+        "Topic :: Utilities"
+    ],
+    keywords="android error cli ai fix log stacktrace",
 )
